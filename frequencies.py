81a05a8277f3e45f57c9d28139e2006f74364967
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2022-10-09 12:39:15 +0100

    done

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
+
+    for c in items:
+        frequencies[str(c)] = 1 + frequencies.get(str(c),0)
+
+
+
     # Your code goes here
     return frequencies
